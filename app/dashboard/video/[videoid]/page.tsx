46a1416324865416f2ce52aefa9e76a1f80065a1
Anav Bordia <abordia01@gmail.com>
2025-03-30 21:15:33 -0700

    explicit pageProps interface

--- app/dashboard/video/[videoid]/page.tsx
+++ app/dashboard/video/[videoid]/page.tsx
@@ -1,15 +1,19 @@
-import { auth } from "@clerk/nextjs/server"
-import { redirect } from "next/navigation"
-import VideoPageClient from "@/app/dashboard/video/[videoid]/video-page-client"
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import VideoPageClient from "@/app/dashboard/video/[videoid]/video-page-client";
 
+interface PageProps {
+  params: {
+    videoid: string;
+  };
+}
 
-export default async function VideoPage({ params }: { params: { videoid: string } }) {
+export default async function VideoPage({ params }: PageProps) {
   const { userId } = await auth();
 
   if (!userId) {
     redirect("/signin");
   }
 
-  return <VideoPageClient videoId={params.videoid} />
+  return <VideoPageClient videoId={params.videoid} />;
 }
-
