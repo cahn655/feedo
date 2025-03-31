776a0a5e8dd1e64c7f51bbf5043ef76c8119828e
Anav Bordia <abordia01@gmail.com>
2025-03-30 19:30:48 -0700

    to fix vercel deployment

--- app/dashboard/video/[videoid]/page.tsx
+++ app/dashboard/video/[videoid]/page.tsx
@@ -2,10 +2,11 @@ import { auth } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation"
 import VideoPageClient from "@/app/dashboard/video/[videoid]/video-page-client"
 
-interface PageProps {
+type PageProps = {
   params: {
     videoid: string
   }
+  searchParams: { [key: string]: string | string[] | undefined }
 }
 
 export default async function VideoPage({ params }: PageProps) {
