2c78a57fff334fd2c332e81a78590b8831eacc4a
Anav Bordia <abordia01@gmail.com>
2025-03-30 21:42:53 -0700

    use promise for parameters

--- app/dashboard/video/[videoid]/page.tsx
+++ app/dashboard/video/[videoid]/page.tsx
@@ -2,18 +2,14 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import VideoPageClient from "@/app/dashboard/video/[videoid]/video-page-client";
 
-interface PageProps {
-  params: {
-    videoid: string;
-  };
-}
-
-export default async function VideoPage({ params }: PageProps) {
+export default async function VideoPage(props: { params: Promise<{ videoid: string }> }) {
   const { userId } = await auth();
 
   if (!userId) {
     redirect("/signin");
   }
 
-  return <VideoPageClient videoId={params.videoid} />;
+  const { videoid } = await props.params;
+
+  return <VideoPageClient videoId={videoid} />;
 }
